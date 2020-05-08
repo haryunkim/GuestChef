5df3c22eaa1736b3b4f68d737105d75a2a41614e
Haryun (Danny) Kim <50666685+haryunkim@users.noreply.github.com>
2020-05-08 09:21:10 -0400

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 	// Define constants - constants cannot be changed after they have been declared.
-	define("DB_HOST", "303.itpwebdev.com");
-	define("DB_USER", "haryunki_db_user");
-	define("DB_PASS", "Iloveitp303");
-	define("DB_NAME", "haryunki_guestchef_db");
+	define("DB_HOST", "HOST");
+	define("DB_USER", "USER");
+	define("DB_PASS", "PASS");
+	define("DB_NAME", "NAME");
 ?>
